0e8156e70d800f25163f672bdee56497a24db2c5
Никита <Ployka322@yandex.ru>
2023-08-10 14:35:44 +0700

    NewPro

---
+++ src/main/java/com/example/testtesttest/TestTestTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtesttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTestTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTestTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testtesttest/TestTestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtesttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTestTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
